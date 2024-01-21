c1d36e828f5b041c2dc9c03ace93df5ff263e1fa
ANKIT JHA <ankitjha01aj@gmail.com>
2024-01-21 17:48:36 +0530

    port specified

--- index.js
+++ index.js
@@ -218,5 +218,5 @@ const calculateOverallPercentage = (marks) => {
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on ${PORT}`);
 });
